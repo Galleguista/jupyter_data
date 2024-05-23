2c6c7c9214b618726d55bfb0a0b589eea92f8e9e
Galleguista-Dev <96153655+Galleguista@users.noreply.github.com>
2024-05-23 14:34:55 -0500

    first commit

---
+++ notebooks/test.py
@@ -0,0 +1,8 @@
+# Carga de las librerías necesarias
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
